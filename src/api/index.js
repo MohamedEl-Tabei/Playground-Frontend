baa54725c29fade1809309a04ef79943c0fc6daa
unknown <mohamedeltab33y@gmail.com>
2024-06-24 23:08:33 +0300

    test time

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-//const REQUEST=axios.create({baseURL:"https://playground-server-m7ug.onrender.com/"})
-const REQUEST=axios.create({baseURL:"http://localhost:5000/"})
+const REQUEST=axios.create({baseURL:"https://playground-server-m7ug.onrender.com/"})
+//const REQUEST=axios.create({baseURL:"http://localhost:5000/"})
 export default REQUEST
 
